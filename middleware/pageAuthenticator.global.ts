2568fae0d5a7e37b840befab0996f66c45f80021
Jefazo <gamerproaso25@gmail.com>
2024-06-08 23:04:56 +0200

    #07 revert auth changes

--- middleware/pageAuthenticator.global.ts
+++ middleware/pageAuthenticator.global.ts
@@ -9,16 +9,16 @@ if(to.path!="/"&&to.path!='/job-search'){
    const storage=localStorage;
    const token=storage.getItem("token");
    const userId=storage.getItem("userId")
-   if(token==null||userId==null){
+   if(token==null||userId==null||!(await useFetch(apiAddress,{server:false, lazy:true, params:{token:token,userId:userId}})).data.value){
       
-      if(!(await $fetch(apiAddress,{retryDelay:500,server:false, lazy:true, params:{token:token,userId:userId}}).catch(()=>false)))
+      
       if(to.path!=="/login"){
       storage.setItem("prevPage",to.path)
       return navigateTo("/login");
       }
 
    }
-
+   
    
 
 }
